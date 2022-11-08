77c7e58aecbb4407e9f5119b6819d1d242df723b
David Ory <david.ory@gmail.com>
2022-11-08 16:46:16 -0500

    bring in ABM files

---
+++ sandag_abm/src/main/python/pythonGUI/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+import py2exe
+
+__author__ = 'wsu'
+
+setup(windows=['./src/main/python/pythonGUI/parameterEditor.py'])
+setup(windows=['./src/main/python/pythonGUI/createStudyAndScenario.py'])
+setup(windows=['./src/main/python/pythonGUI/validatorGUI.py'])
