0fed136747346a20e55fff9d3624c58d13a2987e
Vivek Yadav <vivekyadav2691@gmail.com>
2023-01-16 16:00:33 -0600

    cleanup - RSM assembler

--- sandag_abm/src/main/resources/runRSMAssembler.cmd
+++ sandag_abm/src/main/resources/runRSMAssembler.cmd
@@ -6,7 +6,6 @@ set RSM_SCRIPT_DIR=%3
 set ORG_MODEL_DIR=%4
 set ITERATION=%5
 
-::cd RSM_SCRIPT_DIR
+rem #### assemble trip tables from CT Ramp and Donor model
 "%PYTHON_DIR%/python.exe" "%RSM_SCRIPT_DIR%/rsm_assembler.py" %MAIN_DIR% %ORG_MODEL_DIR% %ITERATION%
-::docker run -v %cd%:/home/mambauser/sandag_rsm -w /home/mambauser/sandag_rsm sandag_rsm python rsm_zone_aggregator.py INPUT_DIR
 
