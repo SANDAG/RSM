4064bfa09e169d78cda5a98c78784c1255f05398
Arash <39972246+arashasadabadi@users.noreply.github.com>
2023-02-07 13:41:57 -0800

    update batch files

--- visualizer/run-visulizer-support.bat
+++ visualizer/run-visulizer-support.bat
@@ -1,7 +1,8 @@
 @echo on
 
 :: set paths
-SET DATA_PIPELINE_PATH=C:\projects\SimWrapper\test\visualizer\pipeline
+SET DATA_PIPELINE_PATH=C:\projects\SimWrapper\RSM_main\visualizer\Pipeline
+SET VISULAZER_PATH=C:\projects\SimWrapper\RSM_main\visualizer
 :: change the directory to data-pipeline-tool folder
 cd /d %DATA_PIPELINE_PATH%
 
@@ -11,8 +12,11 @@ CALL conda env create -f environment.yml
 :: activate the conda environment
 CALL conda activate sandag-rsm-visualizer
 
-:: run the script for data pipeline tool 
-python run.py
+
+
+:: run the support script. 
+cd /d %VISULAZER_PATH%
+python visualizer_support.py "config.yml"
 
 
 
