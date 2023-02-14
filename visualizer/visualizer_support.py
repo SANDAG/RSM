d024eb286a1fa1aeb38f84e8f9df8297aee82a65
Arash <39972246+arashasadabadi@users.noreply.github.com>
2023-02-14 11:45:48 -0800

    Update visualizer_support.py

--- visualizer/visualizer_support.py
+++ visualizer/visualizer_support.py
@@ -62,7 +62,7 @@ def combine_scenarios_summary(scenario_list, mode_summary_file_name, vmt_summary
         summary_df = pd.read_csv(os.path.join(scenario_dir, mode_summary_file_name))
         temp1["tripMode"] = summary_df["tripMode"]
         temp1[scenario] = summary_df["share"]
-    temp1.to_csv(os.path.join(out_dir, mode_summary_file_name))
+    temp1.to_csv(os.path.join(out_dir, mode_summary_file_name), index=False)
 
     temp2 = pd.DataFrame()
     for scenario in scenario_list:
@@ -70,7 +70,7 @@ def combine_scenarios_summary(scenario_list, mode_summary_file_name, vmt_summary
         summary_df = pd.read_csv(os.path.join(scenario_dir,vmt_summary_file_name))
         temp2["class"] = summary_df["class"]
         temp2[scenario] = summary_df["vmt_total"]
-    temp2.to_csv(os.path.join(out_dir, vmt_summary_file_name))
+    temp2.to_csv(os.path.join(out_dir, vmt_summary_file_name), index=False)
 
     temp3 = pd.DataFrame()
     for scenario in scenario_list:
@@ -79,7 +79,7 @@ def combine_scenarios_summary(scenario_list, mode_summary_file_name, vmt_summary
         temp3["type"] = summary_df["type"]
         temp3[scenario] = summary_df["vehicle_miles"]
 
-    temp3.to_csv(os.path.join(out_dir, intrazonal_vmt_file_name))
+    temp3.to_csv(os.path.join(out_dir, intrazonal_vmt_file_name), index=False)
 
 def compare_network_summary (network_comparison_scenario_pair, network_summary_file_name, out_dir):
 
@@ -90,21 +90,22 @@ def compare_network_summary (network_comparison_scenario_pair, network_summary_f
     network_summary2 = pd.read_csv(os.path.join(scenario2_dir,network_summary_file_name) )
     network_summary_dif = (network_summary1.set_index(['ID'])- network_summary2.set_index(['ID'])).reset_index()
     out_file_name = os.path.join(out_dir,  "netowrk_summary_" + network_comparison_scenario_pair[0] + "_" + network_comparison_scenario_pair[1] + "_difference.csv")
-    network_summary_dif.to_csv(out_file_name)
+    network_summary_dif.to_csv(out_file_name, index=False)
 
-def intrazonal_vmt_aggregation(vmt_summary_df_file_name, intrazonal_trips_file_name, intrazonal_vmt_file_name, scenario):
+def intrazonal_vmt_aggregation(vmt_summary_df_file_name, intrazonal_distance_mode_file_name, intrazonal_vmt_file_name, scenario):
 
     scenario_dir = os.path.join("SimWrapper\\data\\processed", scenario)
     vmt_summary_df = pd.read_csv(os.path.join(scenario_dir, vmt_summary_df_file_name))
-    intrazonal_vmt_df = pd.read_csv(os.path.join(scenario_dir, intrazonal_trips_file_name))
+    intrazonal_vmt_df = pd.read_csv(os.path.join(scenario_dir, intrazonal_distance_mode_file_name))
     intrazonal_vmt_df["vmt"] = intrazonal_vmt_df["distance"]
     intrazonal_vmt_df["vmt"][intrazonal_vmt_df['tripMode']=="Shared Ride 2"] = intrazonal_vmt_df["distance"] / 2
     intrazonal_vmt_df["vmt"][intrazonal_vmt_df['tripMode']=="Shared Ride 3+"] = intrazonal_vmt_df["distance"] / 3
     intrazonal_vmt = intrazonal_vmt_df["distance"].sum() 
     vmt = vmt_summary_df["vmt_total"].sum()
-    vmt_df = pd.DataFrame([intrazonal_vmt, vmt], index = ["intrazonal_vmt", "vmt"]).reset_index().set_axis(['type', 'vehicle_miles'], axis=1, inplace=False) 
+    vmt_total = intrazonal_vmt + vmt
+    vmt_df = pd.DataFrame([vmt_total], index = ["total_vmt"]).reset_index().set_axis(['type', 'vehicle_miles'], axis=1, inplace=False) 
     out_file_name = os.path.join(scenario_dir, intrazonal_vmt_file_name)
-    vmt_df.to_csv(out_file_name)
+    vmt_df.to_csv(out_file_name, index=False)
 
 if __name__ == "__main__":
     args = sys.argv
@@ -126,10 +127,10 @@ if __name__ == "__main__":
     mode_summary_file_name = config['inputs']['mode_summary_file_name']
     vmt_summary_file_name = config['inputs']['vmt_summary_file_name']
     compared_scenarios_dir = config['inputs']['compared_scenarios_dir']
-    intrazonal_trips_file_name = config['inputs']['intrazonal_trips_file_name']
+    intrazonal_distance_mode_file_name = config['inputs']['intrazonal_distance_mode_file_name']
     rsm_scenario_list = config['parameters']['rsm_scenario_list']
     base_scenario = config['parameters']['base_scenario']
-    intrazonal_vmt_file_name = config['outputs']['intrazonal_vmt_file_name']
+    total_vmt_file_name = config['outputs']['total_vmt_file_name']
 
     for scenario in (rsm_scenario_list):
         generate_rsm_shapefile(shapefile_dir, cross_reference_mgra_file_name, scenario)
@@ -141,9 +142,9 @@ if __name__ == "__main__":
 
 
     for scenario in (rsm_scenario_list + [base_scenario]):
-        intrazonal_vmt_aggregation(vmt_summary_file_name, intrazonal_trips_file_name, intrazonal_vmt_file_name, scenario)
+        intrazonal_vmt_aggregation(vmt_summary_file_name, intrazonal_distance_mode_file_name, total_vmt_file_name, scenario)
 
-    combine_scenarios_summary(rsm_scenario_list + [base_scenario], mode_summary_file_name, vmt_summary_file_name, intrazonal_vmt_file_name, compared_scenarios_dir)
+    combine_scenarios_summary(rsm_scenario_list + [base_scenario], mode_summary_file_name, vmt_summary_file_name, total_vmt_file_name, compared_scenarios_dir)
 
 #    for pair in list(itertools.combinations(scenario_list, 2)):
 #        compare_network_summary (pair, network_summary_file_name, compared_scenarios_dir)
